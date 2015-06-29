11caec38c0a14dab147ec367f81fe49dddbef3e4
David Inga <david.inga@icloud.com>
2015-06-29 10:30:44 +0200

    removed active admin, devise gem  and active record

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'map#index'
-
+  
   resources :topics, only: [:index, :show]
 
 end
