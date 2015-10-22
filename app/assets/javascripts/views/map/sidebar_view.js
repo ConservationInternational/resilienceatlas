2beea3dea2d6c250788d7ca7dba8871bb100c791
Clara Linos <clara@simbiotica.es>
2015-10-22 12:56:21 +0200

    tabs working.

---
+++ app/assets/javascripts/views/map/sidebar_view.js
@@ -0,0 +1,31 @@
+(function(root) {
+
+  'use strict';
+
+  root.app = root.app || {};
+  root.app.View = root.app.View || {};
+
+  root.app.View.Sidebar = Backbone.View.extend({
+
+    el: '#sidebarView',
+
+    template: HandlebarsTemplates['map/sidebar_tpl'],
+
+    events: {
+    },
+
+    initialize: function(settings) {
+      var opts = settings && settings.options ? settings.options : {};
+      this.options = _.extend({}, this.defaults, opts);
+
+      this.render();
+    },
+
+    render: function() {
+      this.$el.html(this.template);
+      $(document).foundation('tab', 'reflow');
+    }
+
+  });
+
+})(this);
