c7dc0aeb23e1e1352ad4e0d7def197271596a4f0
Clara Linos <clara@simbiotica.es>
2015-10-22 16:39:07 +0200

    new template

--- app/assets/javascripts/views/map/toolbar_view.js
+++ app/assets/javascripts/views/map/toolbar_view.js
@@ -9,6 +9,8 @@
 
     el: '#toolbarView',
 
+    template: HandlebarsTemplates['map/toolbar_tpl'],
+
     events: {
       'click .btn-share' : '_share',
       'click .btn-analyze' : '_analyze'
@@ -17,6 +19,12 @@
     initialize: function(settings) {
       var opts = settings && settings.options ? settings.options : {};
       this.options = _.extend({}, this.defaults, opts);
+
+      this.render();
+    },
+
+    render: function() {
+      this.$el.html(this.template);
     },
 
     _share: function() {
