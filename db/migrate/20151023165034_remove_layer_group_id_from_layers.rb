9af88ad82cc5caab3eb5be84f42b0f4a6c671267
Miguel Mendoza <info@miguelmendoza.dj>
2015-10-27 09:55:55 +0100

    removes migration

--- db/migrate/20151023165034_remove_layer_group_id_from_layers.rb
+++
@@ -1,8 +0,0 @@
-class RemoveLayerGroupIdFromLayers < ActiveRecord::Migration
-  def self.up
-    #remove_column :layers, :layer_group_id
-  end
-  def self.down
-    add_column :layers, :layer_group_id, :integer
-  end
-end
