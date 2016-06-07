38ef1fcc5b50af23d21e401e459522a8857a61be
Clara Linos <clara.linos@gmail.com>
2016-06-07 19:26:09 +0200

    update marker when changing input vaules

--- app/assets/javascripts/admin/map_parser.js
+++ app/assets/javascripts/admin/map_parser.js
@@ -1,37 +1,7 @@
-// jQuery(document).ready(function ($) {
-//   var setMapLocation = function ($fields, geousMap) {
-//     var location = $fields.geousable('getLocation');
-//     geousMap.locations.empty();
-//     geousMap.locations.add(location, { draggable: true });
-//     geousMap.centerOnLocation(location);
-//   };
-//   // select target fieldset
-
-//   //$('[data-geousable]').each(function () {
-//   var el = $('.lat').parent().parent().parent();
-//   $(el).each(function () {
-
-//     var $fields = $(this),
-//         $map = $('<div class="geous-map" style="width:100%;height:300px;"></div>');
-
-//     // initialize the geousable plugin and container element
-//     $fields.geousable({ overwrite: true });
-//     $fields.prepend($map);
-//     $fields.find('input').change(function() {
-//       setMapLocation($fields, gmap);
-//     });
-//     // build a map with the fields' location
-//     geous.gmap.create({
-//       el: $map,
-//       locations: [ $fields.geousable('getLocation') ]
-//     });
-//   });
-// });
-
 jQuery(document).ready(function ($) {
   var marker;
   var mapDiv = '<div id="geous-map" style="width:100%;height:300px;"></div>';
-  var el = $('.lat').parent().parent().parent().append(mapDiv);
+  var el = $('.lat').parent().parent().parent().prepend(mapDiv);
 
   var lat = $('.lat').val() || 5;
   var lng = $('.lng').val() || 35;
@@ -50,7 +20,22 @@ jQuery(document).ready(function ($) {
     });
     marker.addListener('click', toggleBounce);
     marker.addListener('dragend', getPosiiton);
-  }
+
+    setInputListeners();
+  };
+
+  function setInputListeners() {
+    $('.lat').on('change', updatePosition);
+    $('.lng').on('change', updatePosition);
+  };
+
+  function updatePosition(e) {
+    console.log($(e.currentTarget).val());
+    var lat = $('.lat').val() || 5;
+    var lng = $('.lng').val() || 35;
+    var latlng = new google.maps.LatLng(lat, lng);
+    marker.setPosition(latlng);
+  };
 
   function toggleBounce() {
     if (marker.getAnimation() !== null) {
@@ -66,7 +51,6 @@ jQuery(document).ready(function ($) {
 
     lat = $('.lat').val( markerLat );
     lng = $('.lng').val( markerLng );
-
   }
 
   initMap();
