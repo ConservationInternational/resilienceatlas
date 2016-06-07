01a11246e2641435d2207a6a1f78051e5341f518
Clara Linos <clara.linos@gmail.com>
2016-06-07 19:03:02 +0200

    dragabble marker updating fields

--- app/assets/javascripts/admin/map_parser.js
+++ app/assets/javascripts/admin/map_parser.js
@@ -1,30 +1,73 @@
+// jQuery(document).ready(function ($) {
+//   var setMapLocation = function ($fields, geousMap) {
+//     var location = $fields.geousable('getLocation');
+//     geousMap.locations.empty();
+//     geousMap.locations.add(location, { draggable: true });
+//     geousMap.centerOnLocation(location);
+//   };
+//   // select target fieldset
+
+//   //$('[data-geousable]').each(function () {
+//   var el = $('.lat').parent().parent().parent();
+//   $(el).each(function () {
+
+//     var $fields = $(this),
+//         $map = $('<div class="geous-map" style="width:100%;height:300px;"></div>');
+
+//     // initialize the geousable plugin and container element
+//     $fields.geousable({ overwrite: true });
+//     $fields.prepend($map);
+//     $fields.find('input').change(function() {
+//       setMapLocation($fields, gmap);
+//     });
+//     // build a map with the fields' location
+//     geous.gmap.create({
+//       el: $map,
+//       locations: [ $fields.geousable('getLocation') ]
+//     });
+//   });
+// });
+
 jQuery(document).ready(function ($) {
-  var setMapLocation = function ($fields, geousMap) {
-    var location = $fields.geousable('getLocation');
-    geousMap.locations.empty();
-    geousMap.locations.add(location, { draggable: true });
-    geousMap.centerOnLocation(location);
+  var marker;
+  var mapDiv = '<div id="geous-map" style="width:100%;height:300px;"></div>';
+  var el = $('.lat').parent().parent().parent().append(mapDiv);
+
+  var lat = $('.lat').val() || 5;
+  var lng = $('.lng').val() || 35;
+
+  function initMap() {
+    var map = new google.maps.Map(document.getElementById('geous-map'), {
+      zoom: 3,
+      center: {lat: lat, lng: lng}
+    });
+
+    marker = new google.maps.Marker({
+      map: map,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      position: {lat: lat, lng: lng}
+    });
+    marker.addListener('click', toggleBounce);
+    marker.addListener('dragend', getPosiiton);
+  }
+
+  function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
   };
-  // select target fieldset
 
-  //$('[data-geousable]').each(function () {
-  var el = $('.lat').parent().parent().parent();
-  $(el).each(function () {
+  function getPosiiton() {
+    var markerLat = marker.getPosition().lat();
+    var markerLng = marker.getPosition().lng();
 
-    var $fields = $(this),
-        $map = $('<div class="geous-map" style="width:100%;height:300px;"></div>');
+    lat = $('.lat').val( markerLat );
+    lng = $('.lng').val( markerLng );
 
-    // initialize the geousable plugin and container element
-    $fields.geousable({ overwrite: true });
-    $fields.prepend($map);
-    $fields.find('input').change(function() {
-      setMapLocation($fields, gmap);
-    });
-    // build a map with the fields' location
-    geous.gmap.create({
-      el: $map,
-      locations: [ $fields.geousable('getLocation') ]
-    });
-  });
+  }
+
+  initMap();
 });
-
