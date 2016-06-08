74df8767f8dbcfffa59e1e0dd8b3253f9521ce07
Miguel Mendoza <miguel.mendoza@vizzuality.com>
2016-06-08 14:18:10 +0200

    fixes test

--- spec/acceptance/api/v1/layers_spec.rb
+++ spec/acceptance/api/v1/layers_spec.rb
@@ -1,19 +1,31 @@
 require 'acceptance_helper'
 
-resource 'Layers' do
+resource 'Layer' do
   header "Accept", "application/json; application/vnd.api+json"
   header "Content-Type", "application/vnd.api+json"
   header 'Host', 'http://resilienceatlas.org'
   header 'X-CSRF-Token', 'a_valid_CSRF_token'
 
-  let!(:layer) do
+
+
+  let!(:layer_group) do
+    LayerGroup.create!(name: 'environment', id: 1, site_scope: SiteScope.create!(name: 'CIGRP', id: 1))
+  end
+
+  let!(:layers) do
     3.times do |i|
-      Layer.create!(name: "test layer #{i}", slug: "test_layer_#{i}")
+      Layer.create!(name: "test layer #{i}", slug: "test_layer_#{i}", id: i)
     end
   end
 
-  get "/api/layers/" do
+  let!(:agrupation) do
+    3.times do |i|
+      Agrupation.create!(layer_group_id: 1, layer_id: i)
+    end
+  end
 
+
+  get "/api/layers" do
     example_request "Get layers list" do
       expect(status).to eq(200)
       results = JSON.parse(response_body)['data']
