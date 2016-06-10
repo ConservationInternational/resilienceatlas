7662e7090be96f172be81c40015515eb814225da
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-06-10 13:18:54 +0200

    Specific mimtype for downloads

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,8 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+
+Mime::Type.register "application/json", :json, %w( text/x-json application/jsonrequest )
+
+Mime::Type.register "application/pdf", :pdf, [], %w(pdf)
+Mime::Type.register "application/zip", :zip, [], %w(zip)
