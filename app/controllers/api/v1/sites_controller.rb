0ff2e259cab6a7b61715011e1a0ddcd33095d3ff
Tiago Santos <santos.tiago@gmail.com>
2019-06-11 21:11:21 +0100

    Added JWT authentication along with the Devise one

--- app/controllers/api/v1/sites_controller.rb
+++ app/controllers/api/v1/sites_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     class SitesController < ApiController
     include SitesFilters
+    skip_before_action :authenticate_request
+
       def show
         site = SiteScope.find(params[:site_scope])
         render json: site, include: ['site_pages']
