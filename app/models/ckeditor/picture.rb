5bc8e51ea245b09a1dee1c44d7ae9b63b76641a8
Adeel Kamal Malik <adeel_malik867@hotmail.com>
2021-09-20 22:37:41 +0500

    fixed ckeditor issue

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -17,14 +17,7 @@
 #
 
 class Ckeditor::Picture < Ckeditor::Asset
-  has_attached_file :data,
-                    :url  => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                    :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                    :styles => { :content => '800>', :thumb => '118x100#' }
-
-  validates_attachment_presence :data
-  validates_attachment_size :data, :less_than => 2.megabytes
-  validates_attachment_content_type :data, :content_type => /\Aimage/
+  has_one_attached :data
 
   def url_content
     url(:content)
--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -4,22 +4,17 @@ Ckeditor.setup do |config|
   # Load and configure the ORM. Supports :active_record (default), :mongo_mapper and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
-  require 'ckeditor/orm/active_record'
+  require "ckeditor/orm/active_record"
 
   # Allowed image file types for upload.
   # Set to nil or [] (empty array) for all file types
   # By default: %w(jpg jpeg png gif tiff)
-  # config.image_file_types = %w(jpg jpeg png gif tiff)
-
-  # Allowed flash file types for upload.
-  # Set to nil or [] (empty array) for all file types
-  # By default: %w(jpg jpeg png gif tiff)
-  # config.flash_file_types = %w(swf)
+  # config.image_file_types = ["jpg", "jpeg", "png", "gif", "tiff"]
 
   # Allowed attachment file types for upload.
   # Set to nil or [] (empty array) for all file types
   # By default: %w(doc docx xls odt ods pdf rar zip tar tar.gz swf)
-  # config.attachment_file_types = %w(doc docx xls odt ods pdf rar zip tar tar.gz swf)
+  # config.attachment_file_types = ["doc", "docx", "xls", "odt", "ods", "pdf", "rar", "zip", "tar", "swf"]
 
   # Setup authorization to be run as a before filter
   # By default: there is no authorization.
@@ -39,7 +34,7 @@ Ckeditor.setup do |config|
 
   # Customize ckeditor assets path
   # By default: nil
-  # config.asset_path = 'http://www.example.com/assets/ckeditor/'
+  # config.asset_path = "http://www.example.com/assets/ckeditor/"
 
   # To reduce the asset precompilation time, you can limit plugins and/or languages to those you need:
   # By default: nil (no limit)
@@ -49,10 +44,10 @@ Ckeditor.setup do |config|
   # CKEditor CDN
   # More info here http://cdn.ckeditor.com/
   # By default: nil (CDN disabled)
-  # config.cdn_url = '//cdn.ckeditor.com/4.11.1/standard/ckeditor.js'
+  # config.cdn_url = "//cdn.ckeditor.com/4.5.6/standard/ckeditor.js"
 
   # JS config url
   # Used when CKEditor CDN enabled
-  # By default: "ckeditor/config.js"
-  # config.js_config_url = 'ckeditor/config.js'
+  # By default: "/assets/ckeditor/config.js"
+  # config.js_config_url = "/assets/ckeditor/config.js"
 end
