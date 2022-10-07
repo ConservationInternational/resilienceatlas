370fd27a891f17a50b03ec9cb8b65b795c878f62
Alex Zvoleff <azvoleff@conservation.org>
2022-10-06 20:40:48 -0400

    Add node path

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,6 +5,8 @@ set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rben
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
 
+set :default_env, { path: "PATH=$PATH:/home/ubuntu/.nvm/versions/node/v13.7.0/bin" }
+
 set :linked_files, %w{config/database.yml .env}
 set :linked_dirs, %w{downloads public/ckeditor_assets}
 
