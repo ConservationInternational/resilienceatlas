41754c0c88d0901afea19c6c4695e08c57d84689
David Inga <david.inga@vizzuality.com>
2023-02-28 13:29:44 +0100

    added gh-action for frontend test

--- frontend/server.js
+++ frontend/server.js
@@ -10,4 +10,4 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) =>
   res.sendFile(path.join(__dirname, 'build', 'index.html')),
 );
-app.listen(port);
+app.listen(port, () => console.log(`Listening on port ${port}`));
