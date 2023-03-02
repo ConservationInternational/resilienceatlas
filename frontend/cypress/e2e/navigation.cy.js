116cff05921e49e12a1401917e643ee700830982
David Inga <david.inga@vizzuality.com>
2023-03-02 10:51:59 +0100

    added more navigation test

--- frontend/cypress/e2e/navigation.cy.js
+++ frontend/cypress/e2e/navigation.cy.js
@@ -1,4 +1,4 @@
-describe('Navigation', () => {
+describe('Router', () => {
   it('should navigate to the home page', () => {
     cy.visit('/');
     cy.location('pathname').should('eq', '/');
@@ -17,5 +17,39 @@ describe('Navigation', () => {
   it('should navigate to the journeys page', () => {
     cy.visit('/journeys');
     cy.location('pathname').should('eq', '/journeys');
+  });
+});
+
+describe('Main menu', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('clicking on the logo should navigate to the home page', () => {
+    cy.get('.brand-area')
+      .find('a')
+      .click();
+    cy.location('pathname').should('eq', '/');
+  });
+
+  it('clicking on "Journeys" should navigate to the journeys page', () => {
+    cy.get('.nav-area')
+      .find('a[href="/journeys"]')
+      .click();
+    cy.location('pathname').should('eq', '/journeys');
+  });
+
+  it('clicking on "About" should navigate to the about page', () => {
+    cy.get('.nav-area')
+      .find('a[href="/about"]')
+      .click();
+    cy.location('pathname').should('eq', '/about');
+  });
+
+  it('clicking on "Map" should navigate to the map page', () => {
+    cy.get('.nav-area')
+      .find('a[href="/map"]')
+      .click();
+    cy.location('pathname').should('eq', '/map');
   });
 });
