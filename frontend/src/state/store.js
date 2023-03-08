e9e7a6b7a0d1447bb6d7468a9e06428dd78a273b
David Inga <david.inga@vizzuality.com>
2023-03-09 00:09:46 +0100

    renamed middleware

--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -2,13 +2,13 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import * as reducers from './modules';
-import normalizrMiddleware from './middlewares/normalizrMiddleware';
+import normalizerMiddleware from './middleware/normalizerMiddleware';
 
 const composeEnhancer = composeWithDevTools({});
 
 const initStore = createStore(
   combineReducers(reducers),
-  composeEnhancer(applyMiddleware(thunkMiddleware, normalizrMiddleware)),
+  composeEnhancer(applyMiddleware(thunkMiddleware, normalizerMiddleware)),
 );
 
 export default initStore;
