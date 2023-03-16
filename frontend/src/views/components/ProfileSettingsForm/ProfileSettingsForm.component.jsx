f687287b61da75db807c478ce1b712847c1de075
David Inga <david.inga@vizzuality.com>
2023-03-16 12:29:55 +0100

    fixed me and settings profile form

--- frontend/src/views/components/ProfileSettingsForm/ProfileSettingsForm.component.jsx
+++ frontend/src/views/components/ProfileSettingsForm/ProfileSettingsForm.component.jsx
@@ -1,18 +1,11 @@
-import type { FC } from 'react';
 import React from 'react';
 import cx from 'classnames';
-import type { InjectedFormProps } from 'redux-form';
 import { Form, Field } from 'redux-form';
 
 import FormInput from 'views/shared/inputs/FormInput';
 import Loader from 'views/shared/Loader';
-import type { IProfileSettingsForm } from 'state/modules/user';
 
-const ProfileSettingsForm: FC<InjectedFormProps<IProfileSettingsForm>> = ({
-  user,
-  handleSubmit,
-  submitting,
-}) => (
+const ProfileSettingsForm = ({ user, handleSubmit, submitting }) => (
   <Form onSubmit={handleSubmit}>
     <Field component={FormInput} type="email" name="email" label="Email" autoFocus />
 
