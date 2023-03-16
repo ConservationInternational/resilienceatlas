0d9a166885a8c044facd472af3eab79054aadfd4
David Inga <david.inga@vizzuality.com>
2023-03-16 12:20:57 +0100

    added middleware to setup atlas pages

--- frontend/src/utilities/getSubdomain.ts
+++ frontend/src/utilities/getSubdomain.ts
@@ -1,6 +1,15 @@
 import { PORT, isProd } from '../state/utils/api';
 import { getRouterParam } from './routeParams';
 
+export const getSubdomainFromURL = (url: string): string => {
+  // Site scope depends on the domain set in the API
+  const host = url.replace(/(^http(s?):\/\/)|(\.com)$/g, '');
+  const subdomain = host.split('.')[0];
+  // If no subdomain is set or we're in localhost, return null
+  if (!subdomain || subdomain === 'www' || subdomain === 'localhost') return null;
+  return subdomain;
+};
+
 export const getSubdomain = (): string => {
   // Only works on client side
   if (typeof window === 'undefined') return null;
@@ -8,10 +17,7 @@ export const getSubdomain = (): string => {
   const siteScope = getRouterParam('site_scope');
   if (siteScope && !isProd) return siteScope;
   // Site scope depends on the domain set in the API
-  const host = PORT.replace(/(^http(s?):\/\/)|(\.com)$/g, '');
-  const subdomain = host.split('.')[0];
-  // If no subdomain is set or we're in localhost, return null
-  if (!subdomain || subdomain === 'www' || subdomain === 'localhost') return null;
+  const subdomain = getSubdomainFromURL(PORT);
   return subdomain;
 };
 
