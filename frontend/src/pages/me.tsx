57b29fbc7e62ec9b71b9e29f7dfab1232e928b51
David Inga <david.inga@vizzuality.com>
2023-03-16 13:01:22 +0100

    check user authentication on client side

--- frontend/src/pages/me.tsx
+++ frontend/src/pages/me.tsx
@@ -1,29 +1,41 @@
+import { useEffect } from 'react';
 import Link from 'next/link';
 import { Row } from 'react-foundation';
+import { useRouter } from 'next/router';
 import { connect } from 'react-redux';
 
+import { isAuthenticated } from 'utilities/authenticated';
 import EditProfileForm from 'views/components/EditProfileForm';
 import MainLayout from 'views/layouts/main';
 
 import type { NextPageWithLayout } from './_app';
 
-const MePage: NextPageWithLayout = ({ user }) => (
-  <div className="l-content">
-    <Row>
-      <div className="m-user-form">
-        <h2>
-          Edit {user.first_name} {user.last_name}
-        </h2>
+const MePage: NextPageWithLayout = ({ user }) => {
+  const router = useRouter();
+  const authenticated = isAuthenticated(user);
 
-        <EditProfileForm />
+  useEffect(() => {
+    if (!authenticated) router.push('/');
+  }, [authenticated, router]);
 
-        <Link href="/profile-settings">
-          <a>Manage account</a>
-        </Link>
-      </div>
-    </Row>
-  </div>
-);
+  return (
+    <div className="l-content">
+      <Row>
+        <div className="m-user-form">
+          <h2>
+            Edit {user.first_name} {user.last_name}
+          </h2>
+
+          <EditProfileForm />
+
+          <Link href="/profile-settings">
+            <a>Manage account</a>
+          </Link>
+        </div>
+      </Row>
+    </div>
+  );
+};
 
 MePage.Layout = (page) => <MainLayout pageTitle="Me">{page}</MainLayout>;
 
