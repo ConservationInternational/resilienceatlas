975ec1de87c19c093f43c27161e2a49c2b50306c
David Inga <david.inga@vizzuality.com>
2023-03-17 12:26:41 +0100

    updated capistrano and gemfile

--- frontend/config/deploy.rb
+++ frontend/config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, "https://github.com/ConservationInternational/resilienceatlas.git
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
-set :deploy_to, '/home/ubuntu/resilienceatlas-react'
+set :deploy_to, '/home/ubuntu/resilienceatlas-frontend'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -23,7 +23,7 @@ set :deploy_to, '/home/ubuntu/resilienceatlas-react'
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
-set :linked_files, %w{.env}
+set :linked_files, %w{.env.production}
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
@@ -41,16 +41,18 @@ set :keep_releases, 2
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-# RVM
-set :rvm_ruby_version, '2.2.1'
-set :rvm_custom_path, '/usr/share/rvm'
+# Rbenv
+set :rbenv_type, :user
+set :rbenv_ruby, "3.2.1"
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w[rake gem bundle ruby rails]
+set :rbenv_roles, :all
 
 # Yarn
 # set :yarn_target_path, -> { release_path.join('subdir') } # default not set
 set :yarn_flags, '--silent --no-progress'
 # set :yarn_roles, :all                                     # default
 # set :yarn_env_variables, { 'NODE_OPTIONS': '--max-old-space-size=4096' }
-set :yarn_env_variables, { 'REACT_APP_GENERATE_SOURCEMAP': 'false' }
 
 # Passenger
 set :passenger_restart_with_touch, true
