7f398b57133fe86178eb16780066461aa663bd20
Alvaro Leal <pursuance@gmail.com>
2023-03-24 12:46:23 +0100

    Add env variable and adapt journeylist to new API

--- frontend/src/types/journeys.d.ts
+++ frontend/src/types/journeys.d.ts
@@ -1,29 +1,41 @@
-export type Journey = {
-  id: number;
-  title: string;
-  subtitle: string;
-};
-
-export type JourneyItem = Journey & {
-  theme: string;
-  bg: string;
-  bgBig: string;
-};
-
-export type JourneyStep = {
-  number: number;
-  type: string;
-  title: string;
-  subtitle: string;
-  theme: string;
-  background: string;
-  credits: string;
-  creditsUrl: string;
-  content: string;
-};
-
-export type JourneyDetail = Journey & {
-  steps: JourneyStep[];
-};
-
 export type JourneyList = JourneyItem[];
+
+export interface JourneyItem {
+  id: string;
+  type: string;
+  attributes: Attributes;
+  relationships: Relationships;
+}
+
+export interface Attributes {
+  title: string;
+  subtitle: string;
+  theme: string;
+  background_image: BackgroundImage;
+  credits: string;
+  credits_url: string;
+  published: boolean;
+}
+
+export interface BackgroundImage {
+  small: string;
+  medium: string;
+  original: string;
+}
+
+export interface Relationships {
+  journey_steps: JourneySteps;
+}
+
+export interface JourneySteps {
+  data: Datum[];
+}
+
+export interface Datum {
+  id: string;
+  type: Type;
+}
+
+export enum Type {
+  JourneySteps = 'journey_steps',
+}
--- frontend/src/views/components/JourneysIntrolist/JourneysIntrolist.component.tsx
+++ frontend/src/views/components/JourneysIntrolist/JourneysIntrolist.component.tsx
@@ -3,6 +3,15 @@ import cx from 'classnames';
 import Link from 'next/link';
 
 import type { JourneyList } from 'types/journeys';
+import type { JourneyList as StaticJourneyList } from 'types/static-journeys';
+
+const STATIC_JOURNEYS = process.env.NEXT_PUBLIC_STATIC_JOURNEYS === 'true';
+
+type StaticJourneysIntrolistProps = {
+  journeys: StaticJourneyList;
+  journeysLoaded: boolean;
+  loadJourneys: () => void;
+};
 
 type JourneysIntrolistProps = {
   journeys: JourneyList;
@@ -10,7 +19,7 @@ type JourneysIntrolistProps = {
   loadJourneys: () => void;
 };
 
-const JourneysIntrolist: React.FC<JourneysIntrolistProps> = ({
+const JourneysIntrolist: React.FC<StaticJourneysIntrolistProps | JourneysIntrolistProps> = ({
   journeys,
   journeysLoaded,
   loadJourneys,
@@ -18,7 +27,51 @@ const JourneysIntrolist: React.FC<JourneysIntrolistProps> = ({
   useEffect(() => {
     if (!journeysLoaded) loadJourneys();
   }, [journeysLoaded, loadJourneys]);
-
+  if (!STATIC_JOURNEYS) {
+    return (
+      <ul className="m-journey__grid">
+        {journeys.map((journey, i: number) => {
+          const {
+            attributes: {
+              title,
+              theme,
+              background_image: { original: image },
+            },
+            id,
+          } = journey;
+          return (
+            <li className="m-journey__gridelement" data-index={i} key={id}>
+              <div className="text">
+                <span className="title">
+                  <h2>
+                    <Link href={`/journeys/${id}`}>
+                      <a>{title}</a>
+                    </Link>
+                  </h2>
+                </span>
+                <span className="description">
+                  <p>{theme}</p>
+                </span>
+                <Link href={`/journeys/${id}`}>
+                  <a className="btn btn-secondary theme-bg-color">Learn more</a>
+                </Link>
+              </div>
+              <Link href={`/journeys/${id}`}>
+                <a
+                  className="pic"
+                  style={{
+                    backgroundImage: `url(${image})`,
+                  }}
+                >
+                  <span className="background" />
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
   return (
     <ul className="m-journey__grid">
       {journeys.map((j, i) => (
