d6d36adcd7ce88bc90f68ce890f1abdfeff1603a
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2023-03-28 11:59:34 +0200

    Added letter opener in development

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = {host: "localhost", port: 3000}
-  config.action_mailer.delivery_method = :sparkpost
+  config.action_mailer.delivery_method = :letter_opener
 
   config.log_level = :debug
 
