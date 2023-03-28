838064d435eb89ad44750d2fb473cd5d33068db2
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2023-03-28 12:14:43 +0200

    Use BACKEND_URL for mailer host

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: "resilienceatlas.org"}
+  config.action_mailer.default_url_options = {host: URI.parse(ENV.fetch("BACKEND_URL")).host}
   config.action_mailer.delivery_method = :sparkpost
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
