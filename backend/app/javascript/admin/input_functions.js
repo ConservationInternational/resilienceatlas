b96f07b43601747e7d6e0add325732a74c3ca597
martintomas <martintomas.it@gmail.com>
2023-03-31 12:09:58 +0200

    fix: require whole tree of js assets required for admin

--- backend/app/javascript/admin/input_functions.js
+++ backend/app/javascript/admin/input_functions.js
@@ -1,10 +1,10 @@
-export function addRequiredFlagToField(field) {
+export function addRequiredFlagToInput(field) {
     let label = field.find("label");
     field.addClass("required");
     if(label.has("abbr").length === 0) { label.html(label.text() + "<abbr title=\"required\">*</abbr>"); }
 }
 
-export function removeRequiredFlagFromField(field) {
+export function removeRequiredFlagFromInput(field) {
     let label = field.find("label");
     field.removeClass("required");
     label.find("abbr").remove();
