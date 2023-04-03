d1e2163d5fa4856a8a5afcdc81922eeb23da8304
Alvaro Leal <pursuance@gmail.com>
2023-04-03 15:05:55 +0200

    Add API journeys to slider in home

---
+++ frontend/src/views/components/JourneySlider/JourneySlider.container.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import { load as loadJourneys, makeAll as makeAllJourneys } from 'state/modules/journeys';
+
+import JourneySlider from './JourneySlider.component';
+
+const makeMapStateToProps = () => {
+  const getAllJourneys = makeAllJourneys();
+
+  const mapStateToProps = (state) => ({
+    journeys: getAllJourneys(state),
+    journeysLoaded: state.journeys.loaded,
+  });
+
+  return mapStateToProps;
+};
+
+const mapDispatchToProps = {
+  loadJourneys,
+};
+
+export default connect(makeMapStateToProps, mapDispatchToProps)(JourneySlider);
