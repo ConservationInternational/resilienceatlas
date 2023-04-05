460e9fea150283e7d9ff88e0e73cd7c43834f5a9
Alvaro Leal <pursuance@gmail.com>
2023-04-05 18:14:11 +0200

    Fix tests

--- frontend/cypress/e2e/home.cy.js
+++ frontend/cypress/e2e/home.cy.js
@@ -1,6 +1,6 @@
 describe('Home page', () => {
   beforeEach(() => {
-    cy.visit('/');
+    cy.visit('/en/');
   });
 
   it('should have a welcome message', () => {
@@ -10,33 +10,33 @@ describe('Home page', () => {
 
   it('in journeys section should have a link to the journeys page', () => {
     cy.get('.m-discover__bottom')
-      .find('a.btn-primary[href="/journeys"]')
+      .find('a.btn-primary[href="/en/journeys/"]')
       .should('contain', 'More journeys')
       .click();
-    cy.location('pathname').should('eq', '/journeys');
+    cy.location('pathname').should('eq', '/en/journeys/');
   });
 
   it('in explore section should have a link to the map page', () => {
     cy.get('.m-explore')
-      .find('a.btn-primary[href="/map"]')
+      .find('a.btn-primary[href="/en/map/"]')
       .should('contain', 'Go to the map')
       .click();
-    cy.location('pathname').should('eq', '/map');
+    cy.location('pathname').should('eq', '/en/map/');
   });
 
   it('in about section should have a link to the about page', () => {
     cy.get('.m-home-about')
-      .find('a.btn-primary[href="/about"]')
+      .find('a.btn-primary[href="/en/about/"]')
       .should('contain', 'Learn more')
       .click();
-    cy.location('pathname').should('eq', '/about');
+    cy.location('pathname').should('eq', '/en/about/');
   });
 
   it('in about section should have a link to the map page', () => {
     cy.get('.m-home-about')
-      .find('a.btn-primary[href="/map"]')
+      .find('a.btn-primary[href="/en/map/"]')
       .should('contain', 'Analysing the data')
       .click();
-    cy.location('pathname').should('eq', '/map');
+    cy.location('pathname').should('eq', '/en/map/');
   });
 });
