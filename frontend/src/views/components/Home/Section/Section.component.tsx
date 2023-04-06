cb270520d415b03a102c17a6a92c219de7b56914
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-04-06 11:32:43 +0100

    Add a new Home/Section component

---
+++ frontend/src/views/components/Home/Section/Section.component.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import Link from 'next/link';
+import cx from 'classnames';
+
+import type { SectionItem } from 'types/home-section';
+
+type SectionProps = Omit<SectionItem, 'id' | 'position'>;
+
+const Section: React.FC<SectionProps> = ({
+  title,
+  subtitle,
+  button_text,
+  button_url,
+  image,
+  image_position,
+  image_credits,
+  image_credits_url,
+  background_color,
+}) => {
+  return (
+    <div
+      className={cx('m-home-section', {
+        'm-home-section--gradient': image && image_position === 'cover',
+      })}
+      style={{
+        backgroundColor: background_color,
+        backgroundSize: image_position === 'cover' && 'cover',
+        ...(image && image_position === 'cover' && { backgroundImage: `url(${image})` }),
+      }}
+    >
+      <div className="m-home-section__container">
+        <div
+          className={cx('m-home-section__content', {
+            'm-home-section__content--center': image_position === 'cover',
+            'm-home-section__content--left': image_position === 'right',
+            'm-home-section__content--right': image_position === 'left',
+          })}
+        >
+          <h2>{title}</h2>
+          <p>{subtitle}</p>
+          {button_text && button_url && (
+            <Link href={button_url}>
+              <a className="btn btn-primary theme-color">{button_text}</a>
+            </Link>
+          )}
+        </div>
+      </div>
+      {image && image_position !== 'cover' && (
+        <figure
+          className={`m-home-section__figure m-home-section__figure--${image_position}`}
+          style={{
+            backgroundImage: `url(${image})`,
+          }}
+        />
+      )}
+      {image && image_credits && (
+        <p
+          className={cx('m-home-section__credits', {
+            'm-home-section__credits--left': image_position === 'left',
+            'm-home-section__credits--right': image_position !== 'left',
+          })}
+        >
+          <a target="_blank" rel="noopener noreferrer" href={image_credits_url}>
+            {image_credits}
+          </a>
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default Section;
