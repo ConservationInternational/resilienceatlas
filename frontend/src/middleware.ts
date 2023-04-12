13ee11e06d233ecfcdfe5b32bc2a3151188921e8
Alvaro Leal <pursuance@gmail.com>
2023-04-12 17:09:19 +0200

    Unused middleware condition

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -3,8 +3,6 @@ import { PORT } from 'state/utils/api';
 import { getSubdomainFromURL } from 'utilities/getSubdomain';
 import type { NextRequest } from 'next/server';
 
-const PUBLIC_FILE = /\.(.*)$/;
-
 export function middleware(request: NextRequest) {
   // Only when the subdomain is set and the path is /, redirect to the map
   if (!!getSubdomainFromURL(PORT) && request.nextUrl.pathname === '/') {
@@ -12,15 +10,6 @@ export function middleware(request: NextRequest) {
   }
   // Only when the subdomain is set do not allow access
   if (!!getSubdomainFromURL(PORT)) return NextResponse.redirect(new URL('/404', request.url));
-
-  // Avoid adding locale prefixes to api or public files
-  if (
-    request.nextUrl.pathname.startsWith('/_next') ||
-    request.nextUrl.pathname.includes('/api/') ||
-    PUBLIC_FILE.test(request.nextUrl.pathname)
-  ) {
-    return;
-  }
 }
 
 // See "Matching Paths" below to learn more
