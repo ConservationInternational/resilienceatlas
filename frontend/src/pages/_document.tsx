b33caa8f5df2ac80fe0a3b2aba700fe57ca1eed4
Alvaro Leal <pursuance@gmail.com>
2023-04-17 15:11:46 +0200

    Set default locale from locales json

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,15 +1,22 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import type { DocumentContext, DocumentInitialProps } from 'next/document';
+import localesJson from 'locales.config.json';
+
+type Locale = {
+  locale: string;
+  default: boolean;
+  name: string;
+};
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
-
   render(): JSX.Element {
+    const defaultLocale = localesJson.locales.find((l: Locale) => l.default).locale;
     return (
-      <Html lang="en">
+      <Html lang={defaultLocale}>
         <Head />
         <body>
           <div id="root">
