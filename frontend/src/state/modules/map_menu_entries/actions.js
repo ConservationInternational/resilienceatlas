655a2f5c8997f7412ee9c4a32f4ef00da0416a69
Alvaro Leal <pursuance@gmail.com>
2023-04-18 18:23:20 +0200

    Add map menu translation fetch

--- frontend/src/state/modules/map_menu_entries/actions.js
+++ frontend/src/state/modules/map_menu_entries/actions.js
@@ -1,9 +1,13 @@
 import api, { createApiAction } from '../../utils/api';
 import { map_menu_entry } from '../../schema';
+import { toBackendLocale } from 'utilities';
 
 const URL_ENTRIES = '/menu-entries';
 
 // Action constants
 export const LOAD = createApiAction('map_menu_entries/LOAD');
 
-export const load = () => api(LOAD, ({ get }) => get(URL_ENTRIES), { schema: [map_menu_entry] });
+export const load = (locale) =>
+  api(LOAD, ({ get }) => get(URL_ENTRIES, { params: { locale: toBackendLocale(locale) } }), {
+    schema: [map_menu_entry],
+  });
