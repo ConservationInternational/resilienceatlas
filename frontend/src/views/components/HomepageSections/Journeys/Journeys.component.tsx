2ce5f34ab0c8207700025c8b2558c65b1c697fe8
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-04-18 12:41:06 +0100

    Use new Journeys slider component in the homepage

--- frontend/src/views/components/HomepageSections/Journeys/Journeys.component.tsx
+++ frontend/src/views/components/HomepageSections/Journeys/Journeys.component.tsx
@@ -1,96 +1,23 @@
-import React, { useEffect, useMemo } from 'react';
+import React from 'react';
 import Link from 'next/link';
-import Slider from 'react-slick';
-import { T } from '@transifex/react';
 
-import type { JourneyItem } from 'types/journeys';
+import JourneySlider from 'views/components/JourneySlider';
 
 type JourneysProps = {
   title: string;
-  journeys: JourneyItem[];
-  journeysLoaded: boolean;
-  loadJourneys: () => void;
 };
 
-const Journeys: React.FC<JourneysProps> = ({
-  title,
-  journeys: journeysData,
-  journeysLoaded,
-  loadJourneys,
-}) => {
-  useEffect(() => {
-    if (!journeysLoaded) loadJourneys();
-  }, [journeysLoaded, loadJourneys]);
-
-  const journeys = useMemo(
-    () => journeysData.map(({ id, attributes }) => ({ id, ...attributes })),
-    [journeysData],
-  );
-
-  if (!journeysLoaded) return null;
-
+const Journeys: React.FC<JourneysProps> = ({ title }) => {
   return (
     <div className="m-home-journeys" data-cy="homepage-section">
       <div className="m-home-journeys__title">
         <h2>{title}</h2>
       </div>
-      <div className="m-slider" id="sliderView">
-        <Slider
-          className="m-slider__itemlist"
-          slidesToShow={1}
-          centerPadding="120px"
-          draggable
-          infinite
-          arrows
-          centerMode
-          responsive={[
-            {
-              breakpoint: 800,
-              settings: {
-                centerMode: false,
-              },
-            },
-          ]}
-        >
-          {journeys.map((journey) => {
-            const { id, background_image, title, subtitle, credits, credits_url } = journey;
-            return (
-              <li key={id} className="m-slider__item">
-                <span
-                  className="m-slider__image"
-                  style={{
-                    ...(background_image && {
-                      backgroundImage: `url(${journey.background_image?.original})`,
-                    }),
-                  }}
-                />
-                <Link href={`/journeys/${id}`}>
-                  <a>
-                    <div className="title">
-                      {subtitle && <h2>{subtitle}</h2>}
-                      <h3>{title}</h3>
-                    </div>
-                    <span className="journey-link__helper" />
-                  </a>
-                </Link>
-                {credits && credits_url && (
-                  <p className="credits">
-                    <a href={credits_url} target="_blank" rel="noopener noreferrer">
-                      {credits}
-                    </a>
-                  </p>
-                )}
-              </li>
-            );
-          })}
-        </Slider>
-      </div>
+      <JourneySlider />
 
       <div className="m-home-journeys__bottom">
         <Link href="/journeys">
-          <a className="btn btn-primary">
-            <T _str="More journeys" />
-          </a>
+          <a className="btn btn-primary">More journeys</a>
         </Link>
       </div>
     </div>
--- frontend/src/views/components/HomepageSections/Journeys/Journeys.container.js
+++ frontend/src/views/components/HomepageSections/Journeys/Journeys.container.js
@@ -1,21 +1,3 @@
-import { connect } from 'react-redux';
-import { load as loadJourneys, makeAll as makeAllJourneys } from 'state/modules/journeys';
-
 import Journeys from './Journeys.component';
 
-const makeMapStateToProps = () => {
-  const getAllJourneys = makeAllJourneys();
-
-  const mapStateToProps = (state) => ({
-    journeys: getAllJourneys(state),
-    journeysLoaded: state.journeys.loaded,
-  });
-
-  return mapStateToProps;
-};
-
-const mapDispatchToProps = {
-  loadJourneys,
-};
-
-export default connect(makeMapStateToProps, mapDispatchToProps)(Journeys);
+export default Journeys;
