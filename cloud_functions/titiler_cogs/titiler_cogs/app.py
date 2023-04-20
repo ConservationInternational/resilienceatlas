a5da7ab7a4c7e1b12f3afd710eef3c7758c14624
Alicia <alicia.arenzana@gmail.com>
2023-04-20 09:09:43 +0200

    comment new development

--- cloud_functions/titiler_cogs/titiler_cogs/app.py
+++ cloud_functions/titiler_cogs/titiler_cogs/app.py
@@ -3,18 +3,35 @@ from mangum import Mangum
 from titiler.core.factory import TilerFactory
 from titiler.core.errors import DEFAULT_STATUS_CODES, add_exception_handlers
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
 
 logging.getLogger("mangum.lifespan").setLevel(logging.ERROR)
 logging.getLogger("mangum.http").setLevel(logging.ERROR)
 
-app = FastAPI(title="Resilience COG tiler",
-              description="Cloud Optimized GeoTIFF")
+
+# def MosaicPathParams(
+#     mosaic: str = Query(..., description="mosaic name")
+# ) -> str:
+#     """Create dataset path from args"""
+#     # mosaic name should be in form of `{user}.{layername}`
+#     if not re.match(self.mosaic, r"^[a-zA-Z0-9-_]{1,32}\.[a-zA-Z0-9-_]{1,32}$"):
+#         raise HTTPException(
+#             status_code=400,
+#             detail=f"Invalid mosaic name {self.input}.",
+#         )
+
+#         return f"{MOSAIC_BACKEND}{MOSAIC_HOST}/{self.input}.json.gz"
+
+
+# Create FastAPI app
+app = FastAPI(title="Resilience COG tiler", description="Cloud Optimized GeoTIFF")
 
 cog = TilerFactory()
 app.include_router(cog.router, tags=["Cloud Optimized GeoTIFF"])
 
 add_exception_handlers(app, DEFAULT_STATUS_CODES)
+
+# Add health check
 
 
 @app.get("/healthz", description="Health Check", tags=["Health Check"])
@@ -23,4 +40,5 @@ def ping():
     return {"ping": "pong!"}
 
 
+# Create Mangum handler that can be used by AWS Lambda
 handler = Mangum(app, lifespan="off")
