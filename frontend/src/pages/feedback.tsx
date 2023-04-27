55314e4129a3ff86734631f5c10d931747cd038d
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-04-27 10:45:28 +0100

    Add Feedback page

---
+++ frontend/src/pages/feedback.tsx
@@ -0,0 +1,29 @@
+import FullscreenLayout from 'views/layouts/fullscreen';
+
+import type { NextPageWithLayout } from './_app';
+import { getServerSideTranslations } from 'i18n';
+import { useSetServerSideTranslations, withTranslations } from 'utilities/hooks/transifex';
+import type { GetServerSidePropsContext } from 'next';
+
+import FeedbackForm from 'views/components/FeedbackForm';
+
+const Feedback: NextPageWithLayout = ({ translations, setTranslations }) => {
+  useSetServerSideTranslations({ setTranslations, translations });
+
+  return <FeedbackForm />;
+};
+
+Feedback.Layout = (page, translations) => (
+  <FullscreenLayout pageTitle={translations['Feedback']}>{page}</FullscreenLayout>
+);
+
+export default withTranslations(Feedback);
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { translations } = await getServerSideTranslations(context);
+  return {
+    props: {
+      translations,
+    },
+  };
+}
