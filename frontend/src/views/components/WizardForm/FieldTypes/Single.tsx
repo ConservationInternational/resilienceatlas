d0fdb12be5db3748550d074abf24b36caad43c07
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-04-27 15:23:41 +0100

    Wizard Form Radio group can now display custom/other fields

--- frontend/src/views/components/WizardForm/FieldTypes/Single.tsx
+++ frontend/src/views/components/WizardForm/FieldTypes/Single.tsx
@@ -4,7 +4,10 @@ import { Field } from 'redux-form';
 import ErrorMessage from 'views/components/WizardForm/ErrorMessage';
 
 const RadioGroup = (props) => {
-  const { input, meta, answers } = props;
+  const { input, meta, answers, customAnswer } = props;
+  const { name } = input;
+
+  const acceptCustomAnswer = customAnswer?.id && customAnswer?.label;
 
   return (
     <>
@@ -16,14 +19,49 @@ const RadioGroup = (props) => {
           </label>
         </div>
       ))}
+      {acceptCustomAnswer && (
+        <div className="checkbox">
+          <label>
+            <input type="radio" {...input} name={name} value={customAnswer.id} />
+            {customAnswer.label}
+            <Field
+              name={customAnswer.id}
+              component={(customProps) => {
+                const { input: customInput, meta: customInputMeta } = customProps;
+                return (
+                  <>
+                    <input
+                      ref={(input) => {
+                        if (!customInputMeta?.active) return;
+                        input?.focus();
+                      }}
+                      type="text"
+                      {...customInput}
+                    />
+                    <ErrorMessage {...customInputMeta} />
+                  </>
+                );
+              }}
+            />
+          </label>
+        </div>
+      )}
       <ErrorMessage {...meta} />
     </>
   );
 };
 
 const Single = (props) => {
-  const { id: name, answers } = props;
-  return <Field component={RadioGroup} name={name} answers={answers} />;
+  const { id: name, answers, customAnswer, formValues } = props;
+  return (
+    <Field
+      component={RadioGroup}
+      name={name}
+      answers={answers}
+      formValues={formValues}
+      customAnswer={customAnswer}
+    />
+  );
 };
 
 export default Single;
