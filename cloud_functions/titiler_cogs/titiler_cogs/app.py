82651e3f6e58d642e05595ce60b4dd45cfa4848f
Alicia <alicia.arenzana@gmail.com>
2023-05-11 09:35:25 +0200

    added cors middleware

--- cloud_functions/titiler_cogs/titiler_cogs/app.py
+++ cloud_functions/titiler_cogs/titiler_cogs/app.py
@@ -1,40 +1,46 @@
 import logging
 from mangum import Mangum
 from titiler.core.factory import TilerFactory
-from titiler.core.middleware import CacheControlMiddleware
 from titiler.core.errors import DEFAULT_STATUS_CODES, add_exception_handlers
-
+from titiler.core.middleware import CacheControlMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException, Query
 
 logging.getLogger("mangum.lifespan").setLevel(logging.ERROR)
 logging.getLogger("mangum.http").setLevel(logging.ERROR)
+logging.getLogger("rio-tiler").setLevel(logging.ERROR)
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://resilience.org",
+    "http://*.resilience.org",
+    "http://vitalsigns.org",
+    "http://*.vitalsigns.org",
+]
 
-# def MosaicPathParams(
-#     mosaic: str = Query(..., description="mosaic name")
-# ) -> str:
-#     """Create dataset path from args"""
-#     # mosaic name should be in form of `{user}.{layername}`
-#     if not re.match(self.mosaic, r"^[a-zA-Z0-9-_]{1,32}\.[a-zA-Z0-9-_]{1,32}$"):
-#         raise HTTPException(
-#             status_code=400,
-#             detail=f"Invalid mosaic name {self.input}.",
-#         )
-
-#         return f"{MOSAIC_BACKEND}{MOSAIC_HOST}/{self.input}.json.gz"
-
+# Create cog tiler
+cog = TilerFactory()
 
 # Create FastAPI app
 app = FastAPI(title="Resilience COG tiler", description="Cloud Optimized GeoTIFF")
 
-cog = TilerFactory()
 app.include_router(cog.router, tags=["Cloud Optimized GeoTIFF"])
 app.add_middleware(
-        CacheControlMiddleware,
-        cachecontrol="public, max-age=3600",
-        cachecontrol_max_http_code=400, # https://github.com/developmentseed/titiler/pull/444
-        exclude_path={r"/healthz"},
-    )
+    CacheControlMiddleware,
+    cachecontrol="public, max-age=3600",
+    cachecontrol_max_http_code=400,
+    exclude_path={r"/healthz"},
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+    max_age=3600,
+)
+
 
 add_exception_handlers(app, DEFAULT_STATUS_CODES)
 
