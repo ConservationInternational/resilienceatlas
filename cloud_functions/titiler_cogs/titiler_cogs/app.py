1ea6e0be54aea12114ba09661188b2c59a018a45
Tiago Garcia <tiagojsag@gmail.com>
2023-05-17 09:19:20 +0100

    Replace CORS config with regex

--- cloud_functions/titiler_cogs/titiler_cogs/app.py
+++ cloud_functions/titiler_cogs/titiler_cogs/app.py
@@ -10,15 +10,6 @@ logging.getLogger("mangum.lifespan").setLevel(logging.ERROR)
 logging.getLogger("mangum.http").setLevel(logging.ERROR)
 logging.getLogger("rio-tiler").setLevel(logging.ERROR)
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "http://resilience.org",
-    "http://*.resilience.org",
-    "http://vitalsigns.org",
-    "http://*.vitalsigns.org",
-]
-
 # Create cog tiler
 cog = TilerFactory()
 
@@ -27,30 +18,30 @@ app = FastAPI(title="Resilience COG tiler", description="Cloud Optimized GeoTIFF
 
 app.include_router(cog.router, tags=["Cloud Optimized GeoTIFF"])
 app.add_middleware(
-    CacheControlMiddleware,
-    cachecontrol="public, max-age=3600",
-    cachecontrol_max_http_code=400,
-    exclude_path={r"/healthz"},
+	CacheControlMiddleware,
+	cachecontrol="public, max-age=3600",
+	cachecontrol_max_http_code=400,
+	exclude_path={r"/healthz"},
 )
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST"],
-    allow_headers=["*"],
-    max_age=3600,
+	CORSMiddleware,
+	allow_credentials=True,
+	allow_origin_regex='https?://(((\w*)\.)*vitalsigns.org|((\w*)\.)*resilienceatlas.org|localhost(:(\d)*)?)',
+	allow_methods=["GET", "POST"],
+	allow_headers=["*"],
+	max_age=3600,
 )
 
-
 add_exception_handlers(app, DEFAULT_STATUS_CODES)
+
 
 # Add health check
 
 
 @app.get("/healthz", description="Health Check", tags=["Health Check"])
 def ping():
-    """Health check."""
-    return {"ping": "pong!"}
+	"""Health check."""
+	return {"ping": "pong!"}
 
 
 # Create Mangum handler that can be used by AWS Lambda
