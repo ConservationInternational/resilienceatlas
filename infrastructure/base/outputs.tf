5ee84a4bb6b78544718691f25e6cf171bc73e335
Tiago Garcia <tiagojsag@gmail.com>
2023-05-25 16:36:18 +0100

    Fix AWS credentials secret name for GH Actions

--- infrastructure/base/outputs.tf
+++ infrastructure/base/outputs.tf
@@ -1,3 +1,18 @@
 output "route53_zone_name_servers" {
   value = module.route53.route53_zone_name_servers
 }
+
+output "pipeline_user_access_key_id" {
+  value = module.iam.pipeline_user_access_key_id
+  sensitive = true
+}
+
+output "pipeline_user_secret_access_key" {
+  value = module.iam.pipeline_user_access_key_secret
+  sensitive = true
+}
+
+output "pipeline_execution_role" {
+  value = module.iam.pipeline_role_arn
+  sensitive = true
+}
