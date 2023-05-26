f8ddd9d7551b5e77d7d57d3e656c1f7c2b59e9bc
Tiago Garcia <tiagojsag@gmail.com>
2023-05-26 12:57:21 +0100

    Auto-format code

--- infrastructure/base/outputs.tf
+++ infrastructure/base/outputs.tf
@@ -3,16 +3,16 @@ output "route53_zone_name_servers" {
 }
 
 output "pipeline_user_access_key_id" {
-  value = module.iam.pipeline_user_access_key_id
+  value     = module.iam.pipeline_user_access_key_id
   sensitive = true
 }
 
 output "pipeline_user_secret_access_key" {
-  value = module.iam.pipeline_user_access_key_secret
+  value     = module.iam.pipeline_user_access_key_secret
   sensitive = true
 }
 
 output "pipeline_execution_role" {
-  value = module.iam.pipeline_role_arn
+  value     = module.iam.pipeline_role_arn
   sensitive = true
 }
