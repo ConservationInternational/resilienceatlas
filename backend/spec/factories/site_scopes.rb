9320170d91dcc24ea177a7606235188212afa924
Alex Zvoleff <azvoleff@conservation.org>
2026-03-27 07:48:41 -0400

    Test fixes

--- backend/spec/factories/site_scopes.rb
+++ backend/spec/factories/site_scopes.rb
@@ -38,7 +38,7 @@ FactoryBot.define do
     latitude { Faker::Address.latitude }
     longitude { Faker::Address.longitude }
     header_theme { "ci-theme" }
-    zoom_level { rand(1..10) }
+    zoom_level { 3 }
     linkback_text { Faker::Lorem.sentence }
     linkback_url { Faker::Internet.url }
     header_color { Faker::Color.hex_color }
