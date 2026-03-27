b05880a23cacbf46d5ff90421a480ec0aa1afadc
Alex Zvoleff <azvoleff@conservation.org>
2026-03-27 09:02:11 -0400

    Fix favicon migration

--- backend/db/migrate/20260327120000_add_favicon_url_to_site_scopes.rb
+++ backend/db/migrate/20260327120000_add_favicon_url_to_site_scopes.rb
@@ -1,5 +1,5 @@
 class AddFaviconUrlToSiteScopes < ActiveRecord::Migration[7.2]
   def change
-    add_column :site_scopes, :favicon_url, :text
+    add_column :site_scopes, :favicon_url, :text unless column_exists?(:site_scopes, :favicon_url)
   end
 end
