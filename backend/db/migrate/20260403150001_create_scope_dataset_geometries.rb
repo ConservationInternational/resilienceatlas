1204348db930d1fb6fdd6a35049f4f7d56f59dd3
Alex Zvoleff <azvoleff@conservation.org>
2026-04-06 16:05:27 -0400

    Fix migration

--- backend/db/migrate/20260403150001_create_scope_dataset_geometries.rb
+++ backend/db/migrate/20260403150001_create_scope_dataset_geometries.rb
@@ -1,15 +1,23 @@
 class CreateScopeDatasetGeometries < ActiveRecord::Migration[7.2]
-  def change
+  def up
     create_table :scope_dataset_geometries do |t|
       t.references :scope_dataset, null: false, foreign_key: true
       t.string :unit_id, null: false
       t.jsonb :properties, null: false, default: {}
-      t.multi_polygon :geom, srid: 4326, null: false
       t.timestamps
     end
+
+    execute <<-SQL
+      ALTER TABLE scope_dataset_geometries
+        ADD COLUMN geom geometry(MultiPolygon, 4326) NOT NULL;
+    SQL
 
     add_index :scope_dataset_geometries, [:scope_dataset_id, :unit_id], unique: true,
       name: "idx_scope_dataset_geom_dataset_unit"
     add_index :scope_dataset_geometries, :geom, using: :gist
+  end
+
+  def down
+    drop_table :scope_dataset_geometries
   end
 end
