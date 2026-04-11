7734f6a8f23e4c1e52771a88551808215a3dfc78
Alex Zvoleff <azvoleff@conservation.org>
2026-04-11 16:54:39 -0400

    Test fixes

--- frontend/src/views/components/Map/Toolbar/Toolbar.tsx
+++ frontend/src/views/components/Map/Toolbar/Toolbar.tsx
@@ -3,6 +3,7 @@ import cx from 'classnames';
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react';
 import { useSelector } from 'react-redux';
 
+import type { RootState } from 'state/types';
 import ShareModal from 'views/components/ShareModal';
 import SearchArea from './SearchArea';
 import BasemapButton from './BasemapButton';
@@ -14,28 +15,28 @@ const Toolbar: React.FC = () => {
   useMapSettingsSync();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const handleModalToggle = useCallback(() => setIsModalOpen(!isModalOpen), [isModalOpen]);
-  const hasSearch = useSelector((state: any) => state.site.has_search);
+  const hasSearch = useSelector((state: RootState) => state.site.has_search);
 
   return (
     <div className="m-toolbar">
       <ul>
         {hasSearch !== false && (
-        <li className="m-toolbar__item">
-          <Popover>
-            {({ open, close }) => (
-              <div className={cx('m-toolbar__item--search', { 'is-open': open })}>
-                <PopoverButton className="m-toolbar-item--button">
-                  <svg className="icon">
-                    <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#icon-search" />
-                  </svg>
-                </PopoverButton>
-                <PopoverPanel className="m-toolbar-item--panel">
-                  <SearchArea onAfterChange={close} />
-                </PopoverPanel>
-              </div>
-            )}
-          </Popover>
-        </li>
+          <li className="m-toolbar__item">
+            <Popover>
+              {({ open, close }) => (
+                <div className={cx('m-toolbar__item--search', { 'is-open': open })}>
+                  <PopoverButton className="m-toolbar-item--button">
+                    <svg className="icon">
+                      <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#icon-search" />
+                    </svg>
+                  </PopoverButton>
+                  <PopoverPanel className="m-toolbar-item--panel">
+                    <SearchArea onAfterChange={close} />
+                  </PopoverPanel>
+                </div>
+              )}
+            </Popover>
+          </li>
         )}
         <li className="m-toolbar__item m-toolbar__item--gap-after">
           <button type="button" className="btn-share" onClick={handleModalToggle}>
